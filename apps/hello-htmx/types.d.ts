2c14f712c6f90fde11f0f7531496316702063730
Leif Olsen <leif.o.olsen@gmail.com>
2026-05-17 14:33:40 +0200

    Trying out a more compact types.d.ts for hono/jsx

--- apps/hello-htmx/types.d.ts
+++ apps/hello-htmx/types.d.ts
@@ -1,17 +1,27 @@
 import 'hono';
 
 declare module 'hono/jsx' {
-  interface HTMLAttributes {
-    'hx-get'?: string;
-    'hx-post'?: string;
-    'hx-put'?: string;
-    'hx-delete'?: string;
-    'hx-target'?: string;
-    'hx-swap'?: string;
-    'hx-trigger'?: string;
-    'hx-indicator'?: string;
-    'hx-push-url'?: string | boolean;
-    'hx-confirm'?: string;
-    'hx-vals'?: string;
+  namespace JSX {
+    interface HTMLAttributes {
+      [key: `hx-${string}`]: string | boolean;
+    }
   }
 }
+
+// import 'hono';
+//
+// declare module 'hono/jsx' {
+//   interface HTMLAttributes {
+//     'hx-get'?: string;
+//     'hx-post'?: string;
+//     'hx-put'?: string;
+//     'hx-delete'?: string;
+//     'hx-target'?: string;
+//     'hx-swap'?: string;
+//     'hx-trigger'?: string;
+//     'hx-indicator'?: string;
+//     'hx-push-url'?: string | boolean;
+//     'hx-confirm'?: string;
+//     'hx-vals'?: string;
+//   }
+// }
