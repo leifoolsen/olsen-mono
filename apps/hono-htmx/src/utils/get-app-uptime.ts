0838a79aeb027d3b29bfc54cdeb9ce44e428937f
Leif Olsen <leif.o.olsen@gmail.com>
2026-05-25 14:44:18 +0200

    apps/hono-htmx: Uses Intl.DurationFormat to format temporal duration

--- apps/hono-htmx/src/utils/get-app-uptime.ts
+++ apps/hono-htmx/src/utils/get-app-uptime.ts
@@ -1,7 +1,10 @@
-import { SERVER_START_TIME } from './constants';
+import { SERVER_START_TIME } from './constants.ts';
 
 export const getAppUptime = (): string => {
   const now = Temporal.Now.instant();
-  const duration = now.since(SERVER_START_TIME);
-  return `${duration.hours}t ${duration.minutes}m ${duration.seconds}s`;
+  const duration = now.since(SERVER_START_TIME).round({ largestUnit: 'day' });
+
+  return new Intl.DurationFormat('no-NO', {
+    style: 'narrow',
+  }).format(duration);
 };
