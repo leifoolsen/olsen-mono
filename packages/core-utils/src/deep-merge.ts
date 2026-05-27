134d6b6726d6e6b0be9de4aab333454121b1f60f
Leif Olsen <leif.o.olsen@gmail.com>
2026-05-28 00:06:17 +0200

    core-utils: Moved deep-merge from object-builder to core-utils

---
+++ packages/core-utils/src/deep-merge.ts
@@ -0,0 +1,25 @@
+import { isAtomic, isRecord } from '@olsen-mono/core-utils';
+
+/**
+ * Deep merges two objects.
+ * @remarks This function is a support function for the builders and intentionally mutates the target object.
+ * @param target
+ * @param source
+ */
+export function deepMerge(target: Record<PropertyKey, unknown>, source: Record<PropertyKey, unknown>): void {
+  for (const key of Object.keys(source)) {
+    const sourceValue = source[key];
+
+    if (isRecord(sourceValue) && !isAtomic(sourceValue)) {
+      const targetValue = target[key];
+
+      if (!isRecord(targetValue)) {
+        target[key] = {};
+      }
+
+      deepMerge(target[key] as Record<PropertyKey, unknown>, sourceValue);
+    } else {
+      target[key] = sourceValue;
+    }
+  }
+}
