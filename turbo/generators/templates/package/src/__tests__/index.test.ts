77e7fc0c2a99267367e3f0da585431cd84786c63
Leif Olsen <leif.o.olsen@gmail.com>
2026-06-14 17:23:26 +0200

    Renamed test suite to reflect functionality

--- turbo/generators/templates/package/src/__tests__/index.test.ts
+++ turbo/generators/templates/package/src/__tests__/index.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from 'vitest';
 import { hello } from '../';
 
-describe('isError', () => {
+describe('sayHello', () => {
   it('should say hello to {{dashCase name}}', () => {
     expect(hello()).toBe('Hello from {{dashCase name}}!');
   });
