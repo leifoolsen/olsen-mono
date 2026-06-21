fccd22718d6e8a108933844f6ee26edc6accb372
Leif Olsen <leif.o.olsen@gmail.com>
2026-06-21 19:58:44 +0200

    Updated dependencies

--- apps/hono-htmx/src/button.css.d.ts
+++ apps/hono-htmx/src/button.css.d.ts
@@ -1,4 +1,4 @@
 export type Css = never;
-export type CssVariables = '--_transition' | '--color-text-normal-on-base' | '--color-variant-base' | '--color-variant-border' | '--color-variant-hover' | '--color-variant-text' | '--ease-out-2' | '--font-weight-6' | '--radius-2' | '--size-2' | '--size-4';
+export type CssVariables = '--_transition' | '--color-variant-base' | '--color-variant-border' | '--color-variant-text' | '--ease-out-2' | '--font-weight-6' | '--radius-2' | '--size-2' | '--size-4';
 declare const styles: string;
 export default styles;
