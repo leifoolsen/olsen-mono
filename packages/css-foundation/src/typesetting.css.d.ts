1e2257e37e15159ccdf372f09cd506b9fc3ca9e1
Leif Olsen <leif.o.olsen@gmail.com>
2026-06-21 18:40:08 +0200

    Changeset

--- packages/css-foundation/src/typesetting.css.d.ts
+++ packages/css-foundation/src/typesetting.css.d.ts
@@ -1,4 +1,4 @@
 export type Css = never;
-export type CssVariables = '--border-color' | '--brand-text' | '--color-primary-surface' | '--color-primary-text' | '--font-main' | '--font-mono' | '--font-size-0' | '--font-weight-7' | '--fs-base' | '--fs-h1' | '--fs-h2' | '--fs-h3' | '--lh-body' | '--lh-heading' | '--radius-1' | '--size-1' | '--size-2' | '--size-3' | '--size-4' | '--size-5' | '--size-6' | '--system-brand-color' | '--text-muted' | '--text-sublte';
+export type CssVariables = '--border-color' | '--brand-text-color' | '--color-primary-surface' | '--color-primary-text' | '--font-main' | '--font-mono' | '--font-size-0' | '--font-weight-7' | '--fs-base' | '--fs-h1' | '--fs-h2' | '--fs-h3' | '--lh-body' | '--lh-heading' | '--muted-text' | '--radius-1' | '--size-1' | '--size-2' | '--size-3' | '--size-4' | '--size-5' | '--size-6' | '--system-brand-color' | '--text-sublte';
 declare const styles: string;
 export default styles;
