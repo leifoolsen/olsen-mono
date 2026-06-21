3cfe86aaaa0e3610d26e23f225b07ae4af6b39dc
Leif Olsen <leif.o.olsen@gmail.com>
2026-06-21 18:27:10 +0200

    Added surface contrast colors, sutfce-1 and surface-2

--- packages/css-foundation/src/typesetting.css.d.ts
+++ packages/css-foundation/src/typesetting.css.d.ts
@@ -1,4 +1,4 @@
 export type Css = never;
-export type CssVariables = '--color-neutral-border' | '--color-neutral-surface' | '--color-neutral-text' | '--color-neutral-text-muted' | '--color-primary-base' | '--color-primary-border' | '--color-primary-hover' | '--color-primary-surface' | '--color-primary-text' | '--color-text-normal-on-base' | '--color-variant-surface' | '--ease-out-2' | '--font-main' | '--font-mono' | '--font-size-0' | '--font-weight-7' | '--fs-base' | '--fs-h1' | '--fs-h2' | '--fs-h3' | '--lh-body' | '--lh-heading' | '--radius-1' | '--size-2' | '--size-3' | '--size-4' | '--size-5' | '--size-6';
+export type CssVariables = '--border-color' | '--brand-text' | '--color-primary-surface' | '--color-primary-text' | '--font-main' | '--font-mono' | '--font-size-0' | '--font-weight-7' | '--fs-base' | '--fs-h1' | '--fs-h2' | '--fs-h3' | '--lh-body' | '--lh-heading' | '--radius-1' | '--size-1' | '--size-2' | '--size-3' | '--size-4' | '--size-5' | '--size-6' | '--system-brand-color' | '--text-muted' | '--text-sublte';
 declare const styles: string;
 export default styles;
