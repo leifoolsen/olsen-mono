4942dd4c701f10706f65856ca771026b3badd1c5
Leif Olsen <leif.o.olsen@gmail.com>
2026-07-09 20:28:25 +0200

    Initial layout css module

---
+++ packages/css-foundation/src/layouts.css.d.ts
@@ -0,0 +1,4 @@
+export type Css = never;
+export type CssVariables = '--_cols' | '--_gap' | '--_min-width' | '--layout-cols' | '--layout-gap' | '--layout-grid-item-min' | '--space-l' | '--space-m';
+declare const styles: string;
+export default styles;
