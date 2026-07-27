b2d2c45ecc15fce4e0dcf0e1c44e0ede658a32ec
Leif Olsen <leif.o.olsen@gmail.com>
2026-07-27 17:49:25 +0200

    [data-layout="row"]: Removed margin bottom from first element inside container

--- packages/css-foundation/src/layouts.css.d.ts
+++ packages/css-foundation/src/layouts.css.d.ts
@@ -1,4 +1,4 @@
 export type Css = never;
-export type CssVariables = '--_cols' | '--_gap' | '--_min-width' | '--layout-cols' | '--layout-gap' | '--layout-grid-item-min' | '--space-l' | '--space-m';
+export type CssVariables = '--_cols' | '--_min-width' | '--_switch-gap' | '--layout-cols' | '--layout-gap' | '--layout-grid-item-min' | '--space-l' | '--space-m';
 declare const styles: string;
 export default styles;
