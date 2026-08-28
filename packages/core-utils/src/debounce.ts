cbe64b717a49ea3e25586753a762f67dd80c72d9
Leif Olsen <leif.o.olsen@gmail.com>
2026-08-28 12:30:55 +0200

    Added a simple debounce function.

--- packages/core-utils/src/debounce.ts
+++ packages/core-utils/src/debounce.ts
@@ -1,11 +1,17 @@
 /**
+ * "Hack" for (...args: any[]) => void
+ */
+type AnyFunction = (...args: never[]) => void;
+
+/**
  * A simple debounce that executes a function after a given delay in milliseconds. If the callback
  * is called repatedly before the the timer times out, then the old timer is cleared and starts a fresh countdown.
  *
  * @param func - the function to debounce.
  * @param wait - time to wait, in milliseconds, before the function i executed.
  */
-export function debounce<T extends (...args: Parameters<T>) => void>(func: T, wait: number = 300) {
+
+export function debounce<T extends AnyFunction>(func: T, wait: number = 300) {
   let timeoutId: ReturnType<typeof setTimeout> | null = null;
 
   return (...args: Parameters<T>): void => {
