9e13a49756a88e52719179e6d429c479042e0f08
Leif Olsen <leif.o.olsen@gmail.com>
2026-09-07 18:38:49 +0200

    apps/astro-htmx/astro.config.mjs: Improved config for no-build

--- apps/astro-htmx/astro.config.mjs
+++ apps/astro-htmx/astro.config.mjs
@@ -1,12 +1,28 @@
 // @ts-check
-
+// FIX: Import from 'node:fs', not 'node:fs/promises'
+import { readFileSync } from 'node:fs';
 import node from '@astrojs/node';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
+const pkg = JSON.parse(readFileSync(new URL('./package.json', import.meta.url), { encoding: 'utf8' }));
+
+const internalPackages = Object.keys({ ...pkg.dependencies, ...pkg.devDependencies }).filter((name) =>
+  name.startsWith('@olsen-mono/'),
+);
+
+// See: https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: node({
     mode: 'standalone',
   }),
+
+  vite: {
+    optimizeDeps: {
+      exclude: internalPackages,
+    },
+    ssr: {
+      noExternal: internalPackages,
+    },
+  },
 });
