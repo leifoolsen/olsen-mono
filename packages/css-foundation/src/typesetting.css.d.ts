6d06230d7c4d8450402f821865c10e8d19650790
Leif Olsen <leif.o.olsen@gmail.com>
2026-09-08 18:27:34 +0200

    Replace copyfiles with shx

--- packages/css-foundation/src/typesetting.css.d.ts
+++ packages/css-foundation/src/typesetting.css.d.ts
@@ -1,4 +1,4 @@
 export type Css = never;
-export type CssVariables = '--border-color' | '--brand-text-color' | '--font-family-heading' | '--font-family-main' | '--font-family-mono' | '--font-size-l' | '--font-size-m' | '--font-weight-m' | '--font-weight-xl' | '--letter-spacing-m' | '--line-height-m' | '--muted-text' | '--outline-size' | '--space-l' | '--space-m' | '--space-s' | '--space-xs' | '--subtle-text' | '--system-brand-color' | '--text-sublte';
+export type CssVariables = '--border-color' | '--brand-text-color' | '--font-family-heading' | '--font-family-main' | '--font-family-mono' | '--font-size-l' | '--font-size-m' | '--font-weight-m' | '--font-weight-xl' | '--letter-spacing-m' | '--line-height-m' | '--muted-text' | '--outline-size' | '--space-l' | '--space-m' | '--space-s' | '--space-xs' | '--subtle-text' | '--system-brand-color';
 declare const styles: string;
 export default styles;
