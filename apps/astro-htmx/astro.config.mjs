711d1d4c9182431aedc21eb4205767fb2235e602
Leif Olsen <leif.o.olsen@gmail.com>
2026-09-11 20:55:14 +0200

    Removed code related to internal packages. This is now handeled by JIT

--- apps/astro-htmx/astro.config.mjs
+++ apps/astro-htmx/astro.config.mjs
@@ -1,14 +1,6 @@
 // @ts-check
-// FIX: Import from 'node:fs', not 'node:fs/promises'
-import { readFileSync } from 'node:fs';
 import node from '@astrojs/node';
 import { defineConfig } from 'astro/config';
-
-const pkg = JSON.parse(readFileSync(new URL('./package.json', import.meta.url), { encoding: 'utf8' }));
-
-const internalPackages = Object.keys({ ...pkg.dependencies, ...pkg.devDependencies }).filter((name) =>
-  name.startsWith('@olsen-mono/'),
-);
 
 // See: https://astro.build/config
 export default defineConfig({
@@ -16,13 +8,4 @@ export default defineConfig({
   adapter: node({
     mode: 'standalone',
   }),
-
-  vite: {
-    optimizeDeps: {
-      exclude: internalPackages,
-    },
-    ssr: {
-      noExternal: internalPackages,
-    },
-  },
 });
